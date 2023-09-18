ccbb1cf67bff09f61129557b397deee6126317e1
Blast-tutorials <broughtong92@gmail.com>
2023-09-18 03:14:17 +0200

    lesson upload

---
+++ 00-introduction-and-setting-up/main.py
@@ -0,0 +1,4 @@
+import sdl2
+import OpenGL.GL
+
+print("We are running!")
---
+++ 02-becoming-interactive/main.py
@@ -0,0 +1,86 @@
+import ctypes
+import sdl2
+from OpenGL import GL
+
+window = None
+context = None
+
+programRunning = True
+windowWidth = 1024
+windowHeight = 600
+
+def init():
+    global window, context
+
+    if sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING) != 0:
+        print("Unable to initialise SDL!")
+        return -1
+
+    sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MAJOR_VERSION, 4)
+    sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_MINOR_VERSION, 6)
+    sdl2.SDL_GL_SetAttribute(sdl2.SDL_GL_CONTEXT_PROFILE_MASK, sdl2.SDL_GL_CONTEXT_PROFILE_CORE)
+
+    window = sdl2.SDL_CreateWindow(b"Lesson 02 - Becoming Interactive", sdl2.SDL_WINDOWPOS_UNDEFINED, sdl2.SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, sdl2.SDL_WINDOW_RESIZABLE | sdl2.SDL_WINDOW_OPENGL)
+    if not window:
+        print("Unable to create a window!")
+        return -1
+
+    context = sdl2.SDL_GL_CreateContext(window)
+    if not context:
+        print("Unable to create an OpenGL context!")
+        return -1
+
+    majorVersion = ctypes.c_int32()
+    minorVersion = ctypes.c_int32()
+    sdl2.SDL_GL_GetAttribute(sdl2.SDL_GL_CONTEXT_MAJOR_VERSION, ctypes.pointer(majorVersion))
+    sdl2.SDL_GL_GetAttribute(sdl2.SDL_GL_CONTEXT_MINOR_VERSION, ctypes.pointer(minorVersion))
+    if majorVersion.value < 4 or (majorVersion.value == 4 and minorVersion.value < 5):
+        print("Unable to get a recent OpenGL version!")
+        return -1
+
+    print(GL.glGetString(GL.GL_VERSION).decode())
+
+    GL.glClearColor(0.04, 0.23, 0.51, 1.0)
+
+    return 0
+
+def close():
+
+    sdl2.SDL_GL_DeleteContext(context)
+    sdl2.SDL_DestroyWindow(window)
+    sdl2.SDL_Quit()
+
+def handleInput():
+    global programRunning
+
+    event = sdl2.SDL_Event()
+    while sdl2.SDL_PollEvent(ctypes.byref(event)) != 0:
+
+        if event.type == sdl2.SDL_QUIT:
+            programRunning = False
+            return
+
+        elif event.type == sdl2.SDL_KEYDOWN:
+            print("Key pressed down: %s" % (sdl2.SDL_GetKeyName(event.key.keysym.sym).decode()))
+            if event.key.keysym.sym == sdl2.SDLK_ESCAPE:
+                programRunning = False
+                return
+
+def draw():
+
+    GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+    sdl2.SDL_GL_SwapWindow(window)
+
+def main():
+
+    if init() != 0:
+        return
+
+    while programRunning:
+        draw()
+        handleInput()
+
+    close()
+
+if __name__ == "__main__":
+    main()
